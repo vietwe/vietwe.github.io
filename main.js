942dea8a2c1e96399bee6b555a7eabf96e946025
vietwe <31687303+vietwe@users.noreply.github.com>
2017-09-09 15:08:54 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function playStream(idVideoTag, stream) {
 //.then(stream => playStream('localStream', stream));
 
 
-const peer = new Peer({key: 'peerjs', host: 'https://mypeer343.herokuapp.com', secure: true, port: 443});
+const peer = new Peer({key: 'peerjs', host: 'mypeer343.herokuapp.com', secure: true, port: 443});
 
 peer.on('open', id => {
 $('#my-peer').append(id);
